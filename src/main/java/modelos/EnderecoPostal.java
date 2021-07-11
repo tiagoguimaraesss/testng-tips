185a35e43291aeacf7b2f3aa596fa47bc47c1ec3
tiagoguimaraesss <tiago.guimaraes@ulbra.inf.br>
2021-07-11 20:06:57 -0300

    :heavy_plus_sign: :recycle: :art: Criação de obj. EnderecoPostal, refatoração no CT e adição de dependências p/ bind de dados

--- src/main/java/modelos/EnderecoPostal.java
+++ src/main/java/modelos/EnderecoPostal.java
@@ -1,7 +1,19 @@
 package modelos;
 
+import lombok.Getter;
+
+@Getter
 public class EnderecoPostal {
 
-
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
 
 }
--- src/test/java/BuscaEnderecoPorCEPTest.java
+++ src/test/java/BuscaEnderecoPorCEPTest.java
@@ -1,3 +1,4 @@
+import modelos.EnderecoPostal;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -7,15 +8,15 @@ public class BuscaEnderecoPorCEPTest {
 
     @Test
     public void deveriaBuscarEnderecoPorCEPValidoTest() {
-        given().
-            baseUri("https://viacep.com.br").
-            basePath("/ws").
-            pathParam("cep", 90010120).
-        when().
-            get("/{cep}/json").
-        then().
-            statusCode(SC_OK).
-            log().all();
+        EnderecoPostal enderecoPostalDoCEPInformado = given().
+                                                          baseUri("https://viacep.com.br").
+                                                          basePath("/ws").
+                                                          pathParam("cep", 90010120).
+                                                      when().
+                                                          get("/{cep}/json").
+                                                      then().
+                                                          statusCode(SC_OK).
+                                                          extract().as(EnderecoPostal.class);
     }
 
 }
