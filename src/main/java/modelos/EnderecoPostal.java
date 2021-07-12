fa0b69cf38271a188815db4cb452d1fd2308b4ba
tiagoguimaraesss <tiago.guimaraes@ulbra.inf.br>
2021-07-11 21:19:57 -0300

    :heavy_plus_sign: :recycle: Adição de dependência p/ asserção recursiva e refatoração no CT e obj. EnderecoPostal

--- src/main/java/modelos/EnderecoPostal.java
+++ src/main/java/modelos/EnderecoPostal.java
@@ -1,8 +1,12 @@
 package modelos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class EnderecoPostal {
 
     private String cep;
--- src/test/java/BuscaEnderecoPorCEPTest.java
+++ src/test/java/BuscaEnderecoPorCEPTest.java
@@ -1,22 +1,37 @@
 import modelos.EnderecoPostal;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BuscaEnderecoPorCEPTest {
 
-    @Test
-    public void deveriaBuscarEnderecoPorCEPValidoTest() {
-        EnderecoPostal enderecoPostalDoCEPInformado = given().
-                                                          baseUri("https://viacep.com.br").
-                                                          basePath("/ws").
-                                                          pathParam("cep", 90010120).
-                                                      when().
-                                                          get("/{cep}/json").
-                                                      then().
-                                                          statusCode(SC_OK).
-                                                          extract().as(EnderecoPostal.class);
+    @Test(dataProvider = "cepsValidos")
+    public void deveriaBuscarEnderecoPorCEPValidoTest(int cep, EnderecoPostal enderecoPostalEsperado) {
+        EnderecoPostal enderecoPostalAtual = given().
+                                                 baseUri("https://viacep.com.br").
+                                                 basePath("/ws").
+                                                 pathParam("cep", cep).
+                                             when().
+                                                 get("/{cep}/json").
+                                             then().
+                                                 statusCode(SC_OK).
+                                                 extract().as(EnderecoPostal.class);
+
+        assertThat(enderecoPostalAtual).usingRecursiveComparison().isEqualTo(enderecoPostalEsperado);
+    }
+
+    @DataProvider(name = "cepsValidos")
+    private static Object[][] cepsValidos() {
+        EnderecoPostal enderecoPostalEsperado = new EnderecoPostal("90010-120", "Avenida Presidente João Goulart", "",
+                                                                  "Centro Histórico", "Porto Alegre", "RS",
+                                                                   "4314902", "", "51", "8801");
+
+        return new Object[][] {
+                {90010120, enderecoPostalEsperado}
+        };
     }
 
 }
